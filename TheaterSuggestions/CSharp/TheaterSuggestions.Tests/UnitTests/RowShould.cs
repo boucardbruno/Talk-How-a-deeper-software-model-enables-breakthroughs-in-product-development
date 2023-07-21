4c3660148f8b885f9898b03c334473ad14edd9cd
Bruno Boucard <boucard.bruno@free.fr>
2023-07-21 14:29:52 +0200

    clean up

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/UnitTests/RowShould.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using ExternalDependencies.AuditoriumLayoutRepository;
-using ExternalDependencies.ReservationsProvider;
 using NFluent;
 using NUnit.Framework;
 
